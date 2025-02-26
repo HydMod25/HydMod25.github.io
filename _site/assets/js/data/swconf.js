71eeb1b23abd880e948cdcea2a8f7d72fa41ddb1
legrabow <leonard.grabow@web.de>
2025-02-26 07:45:43 +0100

    Change to chirpy layout

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1740551844',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/Participation/',
+      
+        '/Dates/',
+      
+        '/Fees/',
+      
+        '/Accommodation/',
+      
+        '/Contact/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
