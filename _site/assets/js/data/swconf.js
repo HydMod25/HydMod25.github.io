1e79b7554d9c4628d5a8fe78d913e7b3a9f1953d
legrabow <leonard.grabow@web.de>
2025-02-26 12:10:35 +0100

    remove default README

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740551844',resources: [
+    cacheName: 'chirpy-1740568138',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
